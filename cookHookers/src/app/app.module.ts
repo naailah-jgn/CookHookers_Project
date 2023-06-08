5f94fe3f745cf9ec4e266ce0f7c6e9564c19b6d7
NassimSaker <nassimsaker@gmail.com>
2023-06-08 11:09:06 +0200

    (all-recipes): add component all-recipes

--- cookHookers/src/app/app.module.ts
+++ cookHookers/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllRecipesComponent
   ],
   imports: [
     BrowserModule,
